724040a295efd391cfce5618a6fe7e663b0d93d7
gurjas1882 <gurjas1882@gmail.com>
2024-03-10 16:16:11 -0400

    commit #23457927459237489237492374892748923749729472394729472

--- screens/LearnScreen.js
+++ screens/LearnScreen.js
@@ -1,14 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Montserrat_600SemiBold, Montserrat_700Bold_Italic, useFonts } from "@expo-google-fonts/montserrat";
+import TextRecognition from "../components/Learn/TextRecognition";
 
 const LearnScreen = () => {
+	let [fontsLoaded] = useFonts({
+		Montserrat_700Bold_Italic,
+		Montserrat_600SemiBold,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
 	return (
-		<View>
-			<Text>LearnScreen</Text>
-		</View>
+		<SafeAreaView>
+			<Text style={styles.title}>NOTES</Text>
+			<TextRecognition />
+		</SafeAreaView>
 	);
 };
 
 export default LearnScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		color: "black",
+		fontFamily: "Montserrat_700Bold_Italic",
+		fontStyle: "italic",
+	},
+});
